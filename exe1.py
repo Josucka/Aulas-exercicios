ca2e634a79310cae13fd0de5ca7322bd13ab18c2
Josué Barros <83733874+Josucka@users.noreply.github.com>
2021-12-07 23:41:22 -0300

    Update exe1.py

--- exe1.py
+++ exe1.py
@@ -1,4 +1,4 @@
-def arg(p=0,l=0,m=0):
+def arg(p = 0, l = 0, m = 0):
     soma = p + l + m
     print(f'valores somados sao: {soma}')
 
@@ -7,4 +7,4 @@ p = float(input('digite um numero: '))
 l = float(input('digite um numero: '))
 m = float(input('digite um numero: '))
 
-arg(p,l,m)
+arg(p, l, m)
