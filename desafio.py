d435f8abcb661188d263d49c5f01f45921862820
Josué Barros <83733874+Josucka@users.noreply.github.com>
2021-12-08 00:08:52 -0300

    Update desafio.py

--- desafio.py
+++ desafio.py
@@ -1,7 +1,5 @@
-def data(dia,mes,ano):
-    mes[(1,2,3,4,5,6,7,8,9,10,11,12),('janeiro','feverreiro',
-    'março','abril','maio','junho','julho','agosto',
-    'setembro','outubro','novembro','dezembro')]
+def data(dia, mes, ano):
+    mes[(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12), ('janeiro', 'feverreiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro')]
     if dia < 31:
         print() 
     if mes > 1 and mes < 12:
@@ -17,4 +15,4 @@ dia = int(input('Informe o dia de hoje: '))
 mes = int(input('Informe o mês: '))
 ano = int(input('Informe o ano: '))
 print(f'Data: {dia}/{mes}/{ano}')  
-data(dia,mes,ano)
+data(dia, mes, ano)
